f764a05135f4c27e8f27ba34615545f7655b8336
Devlin Nightshade <sudodrksch9dr@proton.me>
2025-12-07 07:55:07 +0800

    before rest3

--- home.nix
+++ home.nix
@@ -1,9 +1,30 @@
 { config, pkgs, ... }:
 
+let
+  dotfiles = "${config.home.homeDirectory}/niri-config/config";
+  create_symlink = path: config.lib.file.mkOutofStoreSymlink path;
+  configs = {
+    niri = "niri";
+    fuzzel = "fuzzel";
+    starship = "starship";
+  };
+in
+
+{
+    # Iterate over xdg configs and map them accordingly
+    xdg.configFile = builtins.mapAttrs (name: subpath: {
+        source = create_symlink "${dotfiles}/${subpath}";
+        recursive = true;
+    }) configs;
+
+}
+
 {
   home.username = "arshes";
   home.homeDirectory = "/home/arshes";
   programs.git.enable = true;
+  programs.fuzzel.enable = true;
+  programs.swaylock.enable = true;
   home.stateVersion = "25.11";
   programs.zsh = {
     enable = true;
